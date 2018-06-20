d8bfd0c7802ce224b8b88fcf43f0352daf12af26
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-20 14:28:40 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,17 +16,6 @@ require 'pry'
 
 def remove_strawberry(contacts)
 
-  contacts = {
-     "Jon Snow" => {
-       name: "Jon",
-       email: "jon_snow@thewall.we",
-       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-     },
-     "Freddy Mercury" => {
-       name: "Freddy",
-       email: "freddy@mercury.com",
-       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-     }
-   }
-   binding.pry
+binding.pry
+
 end
